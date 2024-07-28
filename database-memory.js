488abe6a01b74f719643c349abdb72c3ad6fc364
heliobarros <barros.helio98@gmail.com>
2024-07-28 19:57:25 -0300

    Finalização

--- database-memory.js
+++ database-memory.js
@@ -4,12 +4,28 @@ export class DatabaseMemory {
 
     #videos = new Map()
 
-    create(video){
-        const videoId = randomUUID
-        this.#videos.set()
+    list(){
+       return Array.from(this.#videos.entries()).map((videoArray)=>{
+            const id = videoArray[0]
+            const data = videoArray[1]
+
+            return {
+                id,
+                ...data,
+            }
+       })
     }
 
-    create(id,video){
-        this.#videos.push(video)
+    create(video){
+        const videoId = randomUUID()
+        this.#videos.set(videoId, video)
+    }
+
+    update(id,video){
+        this.#videos.set(id,video)
+    }
+
+    delete(id){
+        this.#videos.delete(id)
     }
 }
--- server.js
+++ server.js
@@ -4,20 +4,46 @@ import { DatabaseMemory } from './database-memory.js'
 
 const server = fastify()
 
-server.post('/videos', () =>{
-    return 'Hello World'
+const database = new DatabaseMemory()
+
+server.post('/videos', (request, reply) =>{
+    
+    const {title, descripiton, duraction} = request.body
+
+    
+    database.create({
+        title: title,
+        description: descripiton,
+        duration: duraction,
+    })
+
+    return reply.status(201).send()
 })
 
 server.get('/videos', () =>{
-    return 'Hello World'
+    const videos = database.list()
+    console.log(videos)
+    return videos
 })
 
-server.put('/videos/:id', () =>{
-    return 'Hello World'
+server.put('/videos/:id', (request, reply) =>{
+    const videoId = request.params.id
+    const {title, descripiton, duraction} = request.body
+
+    database.update(videoId, {
+        title,
+        descripiton,
+        duraction,
+    })
+
+    return reply.status(204).send()
 })
 
-server.delete('/videos/:id', () =>{
-    return 'Hello World'
+server.delete('/videos/:id', (request, reply) =>{
+    const videoId = request.params.id
+    database.delete(videoId)
+
+    return reply.status(204).send()
 })
 
 
